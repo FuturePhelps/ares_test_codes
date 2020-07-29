01520c308a6c61973bbbda01d1884c3a1bb38d4f
FuturePhelps <felbilodeau@gmail.com>
2020-07-28 22:44:12 -0400

    some new tests for relative sizes

--- IRLF_test.py
+++ IRLF_test.py
@@ -1,12 +1,23 @@
 import ares
 import numpy as np
 import matplotlib.pyplot as plt
+from ares.physics.Constants import c
 
 pars = ares.util.ParameterBundle('mirocha2020:univ')
 pop = ares.populations.GalaxyPopulation(**pars)
 
-dust = pop.dust
+freqs, sed = pop.dust_sed(1e10, 1e15, 1000)
 
-dust.L_nu
+plt.loglog(freqs, sed[-1,:,2])
+plt.loglog(pop.dust.frequencies, pop.dust.L_nu[-1,:,2])
+plt.show()
 
-for_fun_1 = dust.Luminosity(4, 1500)
+wave_max = pop.src.wavelengths.max()
+wave = c / 1e13 * 1e8
+L = pop.Luminosity(6, wave)
+pop.dust.L_nu
+L_dust = pop.dust.Luminosity(6, wave)
+print(wave_max)
+print(L)
+print(L_dust)
+print(L_dust > L)

a15e7e98f4636d81790d5302308f0fb6ddbfe974
FuturePhelps <felbilodeau@gmail.com>
2020-08-24 11:13:37 -0400

    cleaned up code

--- full_population_compute.py
+++ full_population_compute.py
@@ -3,23 +3,27 @@ from DustPopulation import DustPopulation
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Create a GalaxyEnsemble object from the parameters
 pars = ares.util.ParameterBundle('mirocha2020:univ')
-
-# For other parameters
 pop = ares.populations.GalaxyPopulation(**pars)
 
-dust = DustPopulation(pop)
+# This is how we access the DustPopulation object
+dust = pop.dust
 
-freqs, SED = dust.dust_sed(1e10, 3e13, 1000)
+# Retrieve the dust SEDs between nu = 1e10 and 3e13 with 1000 sampled frequencies
+freqs, SED = pop.dust_sed(1e10, 3e13, 1000)
 
+# Plot the seds for one galaxy at all sampled redshifts
 fig, ax = plt.subplots(1,1)
 fig.set_size_inches(6.5, 3.7)
 ax.set_xlabel("Frequency (Hz)")
 ax.set_ylabel("Spectral Radiance (ergs / s / Hz)")
 ax.set_title("SED over redshift for one galaxy\nin the ensemble")
 
-for i in range(len(dust.z)):
-    ax.loglog(freqs, SED[10,:,i], label = ('z = %.1f' % dust.z[i]))
+for i in range(dust.Nz):
+    ax.loglog(freqs, SED[-1,:,i], label = ('z = %.1f' % dust.z[i]))
 ax.legend()
-fig.savefig("figures/SED_some_zero_dust_mass_log.png", bbox_inches = 'tight')
-fig.savefig("figures/SED_some_zero_dust_mass_log.pdf", bbox_inches = 'tight')
+
+# Save it in the figures folder as both pdf and png
+fig.savefig("figures/SED_galactic_dust.png", bbox_inches = 'tight')
+fig.savefig("figures/SED_galactic_dust.pdf", bbox_inches = 'tight')
